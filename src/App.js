f4781c45894cd69c5cf1966b2abe20cfc7c9bc6e
BikashSingh-GitHub <46427658+BikashSingh-GitHub@users.noreply.github.com>
2019-03-06 18:36:15 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+//import './App.css';
+import Header from './Header';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+      <h1></h1>
+      <h3 class="heading">You should know how to "PROGRAM" a computer.</h3>
+      <p>It teaches you how to "THINK".</p>
+      <customFooter/>
+      </div>
+
+      // <div>
+    );
+  }
+}
+
+export default App;
